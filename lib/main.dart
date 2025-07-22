1a2fc5cc22f59eb7b993b69701f817112f347851
Edwing Vera <edwingveraduran@gmail.com>
2025-07-21 22:38:16 -0500

    Call injection into app

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:admin/app.dart';
+import 'package:admin/core/injection.dart';
 import 'package:admin/core/services/supabase_service.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SupabaseService.init();
+  setupDependencies();
   runApp(const AdminApp());
 }
